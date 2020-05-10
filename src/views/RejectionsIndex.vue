2056c9cb35298a42e9771a4eeb8a9f8ea2dac66d
ryan <ryanwaszak@gmail.com>
2020-05-10 12:33:58 -0500

    add all working filters except levelReviewedSelect and rejectionSourceSelect

--- src/views/RejectionsIndex.vue
+++ src/views/RejectionsIndex.vue
@@ -3,77 +3,103 @@
     
     <h1>{{ message }}</h1>
     
-    <!-- <input v-model="searchCategory"> -->
-    <button v-on:click="filter()" type="button" class="btn btn-outline-secondary">Filter Options</button>
-    <div v-if="showFilter">
-      <ul class="nav justify-content-center">
-        <form>
-          <div class="form-row">
-            <div class="col">
-              <label for="exampleFormControlSelect1">Category</label>
-              <select class="form-control" id="categorySelect">
-                <option></option>
-                <option v-for="category in categories">{{ category }}</option>
-              </select>
+    <!-- <label>Category</label><input v-model="searchCategory">
+    <br>
+    <label>Product</label><input v-model="searchProduct"> -->
+    <button v-on:click="filterOptions()" type="button" class="btn btn-outline-secondary">Hide/Show Filters</button>
+      <div v-if="showFilter">
+        <ul class="nav justify-content-center">
+          <form>
+            <div class="form-row">
+              <div class="col">
+                <label for="exampleFormControlSelect1">Category</label>
+                <select class="form-control" id="categorySelect" v-model="categorySelect">
+                  <option>All</option>
+                  <option v-for="category in categories">{{ category }}</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleFormControlSelect1">Sub Category</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="subCategorySelect">
+                  <option>All</option>
+                  <!-- <option>try to only display sub categories that match category?</option> -->
+                  <option>Best Practices</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleFormControlSelect1">Product</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="productSelect">
+                  <option>All</option>
+                  <option v-for="product in products">{{ product }}</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleFormControlSelect1">Office</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="officeSelect">
+                  <option>All</option>
+                  <option>Norristown</option>
+                </select>
+              </div>
             </div>
-            <div class="col">
-              <label for="exampleFormControlSelect1">Sub Category</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option>try to only display sub categories that match category?</option>
-              </select>
+            <div class="form-row">
+              <div class="col">
+                <label for="exampleFormControlSelect1">State</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="stateSelect">
+                  <option>All</option>
+                  <option>PA</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleFormControlSelect1">AHJ</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="ahjSelect">
+                  <option>All</option>
+                  <!-- <option>Only pull from seeded AHJs</option> -->
+                  <option>Borough of Middletown (PA)</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleFormControlSelect1">Level Reviewed</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="levelReviewedSelect">
+                  <option>All</option>
+                  <option v-for="level in levelsReviewed"> {{ level }}</option>
+                </select>
+              </div>
+              <div class="col">
+                <label for="exampleFormControlSelect1">Rejection Source</label>
+                <select class="form-control" id="exampleFormControlSelect1" v-model="rejectionSourceSelect">
+                  <option>All</option>
+                  <option v-for="rejectionSource in rejectionSources">{{ rejectionSource}}</option>
+                </select>
+              </div>
             </div>
-            <div class="col">
-              <label for="exampleFormControlSelect1">Product</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option v-for="product in products">{{ product }}</option>
-              </select>
-            </div>
-            <div class="col">
-              <label for="exampleFormControlSelect1">Office</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option>Only pull from seeded offices</option>
-              </select>
-            </div>
-          </div>
-          <div class="form-row">
-            <div class="col">
-              <label for="exampleFormControlSelect1">State</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option>Only pull from seeded states</option>
-              </select>
-            </div>
-            <div class="col">
-              <label for="exampleFormControlSelect1">AHJ</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option>Only pull from seeded AHJs</option>
-              </select>
-            </div>
-            <div class="col">
-              <label for="exampleFormControlSelect1">Level Reviewed</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option v-for="level in levelsReviewed"> {{ level }}</option>
-              </select>
-            </div>
-            <div class="col">
-              <label for="exampleFormControlSelect1">Rejection Source</label>
-              <select class="form-control" id="exampleFormControlSelect1">
-                <option></option>
-                <option v-for="rejectionSource in rejectionSources">{{ rejectionSource}}</option>
-              </select>
-            </div>
-          </div>
-        </form>
-      </ul>
-    </div>
+              <br>
+              <button v-on:click="applyFilter()" type="button" class="btn btn-outline-secondary">Apply Filter</button> 
+              <!-- display selected options for testing -->
+              <p>Category: {{ categorySelect }}</p>     
+              <p>Product: {{ productSelect }}</p>     
+          </form>
+        </ul>
+      </div>
     <hr>
-    <!-- <div v-for="rejection in rejections"> -->
-    <div v-for="rejection in filterBy(rejections, searchCategory, 'category')">
+    <!-- <div v-for="rejection in rejections">
+      <div v-if="rejection.category === categorySelect">
+        <p><a v-bind:href="`/rejections/${rejection.id}`"> id: {{ rejection.id }}</a></p>
+        <p>installation: {{ rejection.installation }}</p>
+        <p>category: {{ rejection.category }}</p>
+        <p>sub_category: {{ rejection.sub_category }}</p>
+        <p>product: {{ rejection.product }}</p>
+        <p>office: {{ rejection.office }}</p>
+        <p>state: {{ rejection.state }}</p>
+        <p>ahj: {{ rejection.ahj }}</p>
+        <p>note: {{ rejection.note }}</p>
+        <p>level_reviewed: {{ rejection.level_reviewed }}</p>
+        <p>rejection_source: {{ rejection.rejection_source }}</p>
+        <p>corrections_uploaded: {{ rejection.corrections_uploaded }}</p>
+        <hr>
+        <hr>
+      </div>
+    </div> -->
+    <div v-for="rejection in filteredRejections">
       <p><a v-bind:href="`/rejections/${rejection.id}`"> id: {{ rejection.id }}</a></p>
       <p>installation: {{ rejection.installation }}</p>
       <p>category: {{ rejection.category }}</p>
@@ -103,9 +129,19 @@ export default {
   mixins: [Vue2Filters.mixin],
   data: function() {
     return {
-      message: "Welcome to the index page!",
+      message: "All Entries Live Here",
+      categorySelect: "All",
+      subCategorySelect: "All",
+      productSelect: "All",
+      officeSelect: "All",
+      ahjSelect: "All",
+      stateSelect: "All",
+      levelReviewedSelect: "All",
+      rejectionSourceSelect: "All",
       rejections: [],
+      filteredRejections: [],
       searchCategory: "",
+      searchProduct: "",
       showFilter: false,
       categories: [
         "Design Electrical",
@@ -148,10 +184,56 @@ export default {
     });
   },
   methods: {
-    filter: function() {
+    filterOptions: function() {
       this.showFilter = !this.showFilter;
+    },
+    applyFilter: function() {
+      console.log('applying filter...');
+      // reset filteredRejections array to unfiltered rejections array
+      this.filteredRejections = this.rejections;
+
+      if (this.categorySelect !== 'All') {
+        const filtered = this.filteredRejections.filter(entry => entry.category === this.categorySelect);
+        // console.log(filtered);
+        this.filteredRejections = filtered;
+      }
+      if (this.stateSelect !== 'All') {
+        const filtered = this.filteredRejections.filter(entry => entry.state === this.stateSelect);
+        // console.log(filtered);
+        this.filteredRejections = filtered;
+      }
+      if (this.productSelect !== 'All') {
+        const filtered = this.filteredRejections.filter(entry => entry.product === this.productSelect);
+        // console.log(filtered);
+        this.filteredRejections = filtered;
+      }
+      if (this.subCategorySelect !== 'All') {
+        const filtered = this.filteredRejections.filter(entry => entry.sub_category === this.subCategorySelect);
+        // console.log(filtered);
+        this.filteredRejections = filtered;
+      }
+      if (this.officeSelect !== 'All') {
+        const filtered = this.filteredRejections.filter(entry => entry.office === this.officeSelect);
+        // console.log(filtered);
+        this.filteredRejections = filtered;
+      }
+      if (this.ahjSelect !== 'All') {
+        const filtered = this.filteredRejections.filter(entry => entry.ahj === this.ahjSelect);
+        // console.log(filtered);
+        this.filteredRejections = filtered;
+      }
     }
   }
 };
 
+
+// when clicking apply filter:
+
+// v-on:change instead of clicking apply filter for select box
 </script>
+
+
+
+levelReviewedSelect
+rejectionSourceSelect
+
