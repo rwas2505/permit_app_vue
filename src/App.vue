24ea1691c79182c7740c116f5d47150da33ad8f1
ryan <ryanwaszak@gmail.com>
2020-05-11 21:12:42 -0500

    add Chart dropdown to navbar

--- src/App.vue
+++ src/App.vue
@@ -22,17 +22,17 @@
           <li class="nav-item">
             <a class="nav-link" href="/rejections/new">New</a>
           </li>
-          <!-- <li class="nav-item dropdown">
+          <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Dropdown
+              Charts
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Something else here</a>
+              <a class="dropdown-item" href="/data/location">Rejections By State</a>
+              <a class="dropdown-item" href="/data/category">Rejections By Category</a>
+              <!-- <div class="dropdown-divider"></div> -->
+              <a class="dropdown-item" href="/data/product">Rejections By Product</a>
             </div>
-          </li> -->
+          </li>
         </ul>
         <!-- <form class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="categoryFilter">
