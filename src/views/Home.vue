0d2017de1b6ec5b5be9fd9f5a29a8f3dd93930ca
ryan <ryanwaszak@gmail.com>
2020-05-12 20:45:52 -0500

    add anchor tags to home page, final commit befor implementing theme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <a class="nav-link" href="/rejections/new">Enter a New Rejection</a>
+    <a class="nav-link" href="/rejections">View All Rejections</a>
   </div>
 </template>
 
