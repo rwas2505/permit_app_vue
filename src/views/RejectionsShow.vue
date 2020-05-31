94472d130855a8405e531bd73e4641797f8efcc9
ryan <ryanwaszak@gmail.com>
2020-05-30 20:56:37 -0500

    format RejectionsShow.vue to display nested categories and subcategories with styling

--- src/views/RejectionsShow.vue
+++ src/views/RejectionsShow.vue
@@ -5,27 +5,26 @@
         <h3> <a v-bind:href="`/rejections`" class="fas fa-clipboard-check" style="font-size: 48px; color: Dodgerblue;"></a></h3>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item" style="background-color: #C0C0C0;">id: {{ rejection.id }}</li>
+        <li class="list-group-item" style="background-color: #C0C0C0;">REJECTION ID: {{ rejection.id }}</li>
         <li class="list-group-item" style="background-color: #C0C0C0;">product: {{ rejection.product }}</li>
         <li class="list-group-item" style="background-color: #C0C0C0;">level_reviewed: {{ rejection.level_reviewed }}</li>
         <li class="list-group-item" style="background-color: #C0C0C0;">rejection_source: {{ rejection.rejection_source }}</li>
         <li class="list-group-item" style="background-color: #C0C0C0;">corrections_uploaded: {{ rejection.corrections_uploaded }}</li>
         <li class="list-group-item" style="background-color: #C0C0C0;">job_id: {{ rejection.job_id }}</li>
       </ul>
+      <hr>
       <div v-for="(category, index) in rejection.categories">
         <div class="card-header" style="background-color: #343a40; color: #C0C0C0;">
-          <h3> CATEGORY {{index + 1}}</h3>
+          <h3> Category {{index + 1}}: {{category.name}}</h3><h6>id:{{category.id}}</h6>
         </div>
         <ul class="list-group list-group-flush">
-          <li class="list-group-item" style="background-color: #C0C0C0;">id: {{ category.id}}</li>
-          <li class="list-group-item" style="background-color: #C0C0C0;">name: {{ category.name}}</li>
-        </ul>
-        <hr>
-        <div v-for="subcategory in rejection.subcategories">
+        <div v-for="(subcategory, index) in rejection.subcategories">
           <div v-if="subcategory.category_id === category.id">
-            <p>Subcategory:{{subcategory}}</p>
+            <li class="list-group-item" style="background-color: #C0C0C0;">Subcategory {{index + 1}}: {{subcategory.name}}</li>
           </div>
         </div>
+        </ul>
+        <hr>
       </div>
     </div>
   </div>
